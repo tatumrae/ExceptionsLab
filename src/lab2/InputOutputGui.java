b6c4911e908e0f9b0772bd5c7184ac184f3c9032
tatum <tatum@192.168.1.145>
2017-04-04 19:52:10 -0500

    Final

--- src/lab2/InputOutputGui.java
+++ src/lab2/InputOutputGui.java
@@ -17,13 +17,14 @@ public class InputOutputGui {
     }
 
     public void startConversation() {
-
+        String fullName = JOptionPane.showInputDialog("Enter full name:");
+        String lastName = "";
+        
         try {
-            String fullName = JOptionPane.showInputDialog("Enter full name:");
-            String lastName = "";
             lastName = nameService.extractLastName(fullName);
-            String msg = "Your last name is: " + lastName;
+            String msg = "Assuming you entered correctly, your last name is: " + lastName;
             JOptionPane.showMessageDialog(null, msg);
+            
         } catch (IllegalArgumentException iae) {
             JOptionPane.showMessageDialog(null, iae.getMessage());
         }
